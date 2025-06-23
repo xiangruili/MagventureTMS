9241cb4fb82a03f5ec5ca32de71ccd0b44773e5f
xiangruili <xiangrui.li@gmail.com>
2025-06-23 11:01:41 -0400

    Update TMS_GUI.m

--- TMS_GUI.m
+++ TMS_GUI.m
@@ -1,39 +1,38 @@
 function TMS_GUI(varargin)
 % TMS_GUI shows and controls the Magventure TMS system. 
 % 
-% While TMS class works independently in code or command line, TMS_GUI is only an
-% interface to show and control the stimulator through TMS class.
+% While TMS class works independently in script or command line, TMS_GUI is only
+% an interface to show and control the stimulator through TMS class.
 % 
 % The control panel contains help information for each parameter and item. By
-% hovering mouse on an item for a second, the help will show up.
+% hovering mouse onto an item for a second, the help will show up.
 % 
 % The Coil Status panel is information only, and the coil temperature and di/dt
 % will update after stimulation is applied.
 % 
-% The Basic Control contains Amplitude, Mode, Waveform and Current Direction. The
-% Burst Parameter panel is active only for Waveform of Biphasic Burst.
+% The Basic Control panel contains common basic parameters. The Burst Parameter
+% panel is active only for Waveform of Biphasic Burst. The Train Control panel
+% contains the parameters for train stimulation.
 % 
-% Any Basic and Burst parameter change will be effective immediately at the
-% stimulator, while the Train parameters will be sent only after "Set Train"
-% button is pressed.
+% All parameter change will be effective immediately at the stimulator.
 %
 % The "Motor Threshold" button will start motor threshold estimation. See help
-% for motorThreshold for details.
+% for motorThreshold/m for details.
 %
 % From File menu, the parameters can be saved to a file, so they can be loaded
 % for the future sessions. The "Load" function will send all parameters to the
-% stimulator. Then once the desired amplitude is set, it is ready to fire a
-% "Single pulse" or "Start Train".
+% stimulator. Then once the desired amplitude is set, it is ready to "Trig" a
+% pulse/burst or "Start Train". This is the easy and safe way to set up all
+% parameters.
 % 
-% From Serial menu, one can Resync status from the stimulator, in case a
+% From Serial menu, one can Resync status from the stimulator, in case any
 % parameter is changed at the stimulator (strongly discouraged). One can also
-% Disconnect the serial connection to the stimulator, and this is necessary if
-% another program, e.g. E-Prime, will trigger the stimulation after all
-% parameters are set.
+% Disconnect the serial connection to the stimulator, as this is necessary if
+% another program, e.g. E-Prime, will trigger the stimulation.
 % 
 %  See also motorThreshold
  
-% 250106 xiangrui.li@gmail.com
+% 250106 started by xiangrui.li@gmail.com
  
 fh = findall(0, 'Type', 'figure', 'Tag', 'MagventureGUI');
 if isempty(fh) && nargin, return; end
@@ -53,20 +52,22 @@ hs.waveform.Value = T.waveform;
 hs.currentDirection.Items = T.curDirs.values;
 hs.currentDirection.Value = T.currentDirection;
 hs.burstPulses.Value = T.burstPulses;
+hs.BARatio.Value = T.BARatio;
+set([hs.BARatio hs.BARatioL], 'Enable', OnOff(T.mode=="Twin"));
 hs.IPI.Value = T.IPI;
 hs.IPI.Limits = T.IPIs([end 1]);
 hs.CoilType.Value = T.info.CoilType;
 hs.temperature.Value = T.temperature;
-hs.didt.Value = T.didt(1);
+hs.didt.Value = sprintf('%i  %i', T.didt);
 hs.RepRate.Value = T.train.RepRate;
 hs.RepRate.Limits = T.RATEs([1 end]);
 hs.PulsesInTrain.Value = T.train.PulsesInTrain;
 hs.NumberOfTrains.Value = T.train.NumberOfTrains;
 hs.ITI.Value = T.train.ITI;
 hs.PriorWarningSound.Value = T.train.PriorWarningSound;
-hs.TotalTime.Value = T.train.TotalTime;
+hs.trainTime.Value = T.info.trainTime;
  
-if T.train.isRunning, hs.fireTrain.Text = "Stop Train";
+if T.info.trainRunning, hs.fireTrain.Text = "Stop Train";
 else, hs.fireTrain.Text = "Start Train";
 end
  
@@ -103,10 +104,9 @@ if ~isvalid(T), fh = ancestor(h, "figure"); fh.Name = "NotConnected"; end
 %% Create GUI
 function fh = createGUI()
 % Create uifigure and hide until all components are created
-fh = uifigure('Visible', 'off', 'AutoResizeChildren', 'off', 'Name', '', ...
-    'Position', [100 100 489 406], 'Resize', 'off', 'Tag', 'MagventureGUI');
+fh = uifigure('Visible', 'off', 'AutoResizeChildren', 'off', 'Icon', ones(1,1,3)*0.95, ...
+    'Name', '', 'Position', [100 100 489 406], 'Resize', 'off', 'Tag', 'MagventureGUI');
 CLN = onCleanup(@()set(fh,'Visible','on')); % Show figure after done or error
-try fh.Icon = fullfile(fileparts(mfilename('fullpath')), 'CoilIcon.png'); end %#ok
 
 cb = @(tag){@guiCallback tag};
 hFile = uimenu(fh, 'Text', '&File');
@@ -124,100 +124,113 @@ uimenu(hHelp, 'Text', 'Help about TMS class', 'MenuSelectedFcn', 'doc TMS');
 uimenu(hHelp, 'Text', 'Help about TMS_GUI', 'MenuSelectedFcn', 'doc TMS_GUI');
  
 uilabel(fh, 'Text', 'Disabled', 'FontWeight', 'bold', ...
-    'HorizontalAlignment', 'center', 'BackgroundColor', 'r', 'Position', [31 354 62 32]);
+    'HorizontalAlignment', 'center', 'BackgroundColor', 'r', 'Position', [31 362 62 32]);
 hs.enabled = uilabel(fh, 'Text', 'Enabled', 'FontWeight', 'bold', 'Visible', 'off', ...
-    'HorizontalAlignment', 'center', 'BackgroundColor', 'g', 'Position', [31 354 62 32]);
+    'HorizontalAlignment', 'center', 'BackgroundColor', 'g', 'Position', [31 362 62 32]);
  
 h = uibutton(fh, 'push');
 h.Text = char(11096); % char(0x23FB) for power button
 h.FontSize = 14;
-h.Position = [94 354 32 32];
+h.Position = [94 362 32 32];
 h.ButtonPushedFcn = cb("enable");
 h.Tooltip = {'Push to enable/disable stimulation'};
  
 hs.fire = uibutton(fh, 'push', 'ButtonPushedFcn', cb("firePulse"), ...
-    'FontWeight', 'bold', 'Text', 'Single Pulse', ...
-    'Tooltip', {'Fire a pulse or burst'}, 'Position', [158 354 98 32]);
+    'FontWeight', 'bold', 'Text', 'Trig', ...
+    'Tooltip', {'Trigger a pulse or burst'}, 'Position', [158 362 98 32]);
  
 uibutton(fh, 'push', 'ButtonPushedFcn', @(~,~)motorThreshold, 'Text', 'Motor Threshold', ...
-    'Tooltip', {'Start motor threshold estimate'}, 'Position', [158 310 98 32]);
+    'Tooltip', {'Start motor threshold estimate'}, 'Position', [158 322 98 32]);
 
 uibutton(fh, 'push', 'ButtonPushedFcn', @EMG_check, 'Text', 'EMG Check', ...
-    'Tooltip', {'Show continuous EMG trace'}, 'Position', [31 310 88 32]);
+    'Tooltip', {'Show continuous EMG trace'}, 'Position', [31 322 88 32]);
 
 % Coil panel
 hPanel = uipanel(fh, 'TitlePosition', 'centertop', 'Title', 'Coil Status', ...
-    'FontWeight', 'bold', 'Position', [296 267 171 122]);
+    'FontWeight', 'bold', 'Position', [280 267 182 122]);
  
 % coil type
 h = uilabel(hPanel);
 h.HorizontalAlignment = 'right';
-h.Position = [3 69 77 22];
+h.Position = [13 70 77 22];
 h.Text = 'Type/Number';
-hs.CoilType = uieditfield(hPanel, 'text', 'Position', [85 69 77 22], ...
+hs.CoilType = uieditfield(hPanel, 'text', 'Position', [95 70 77 22], ...
     'Editable', 'off', 'BackgroundColor', [0.9 0.9 0.9], ...
     'Tooltip', {'Connected coil type or number'});
  
 % temperature
 h = uilabel(hPanel);
 h.HorizontalAlignment = 'right';
-h.Position = [4 38 121 22];
+h.Position = [14 40 121 22];
 h.Text = 'Temperature (°C)';
 h.Tooltip = {'Coil temperature in Celsius'};
 hs.temperature = uieditfield(hPanel, 'numeric', 'Editable', 'off', ...
-    'BackgroundColor', [0.9 0.9 0.9], 'Tooltip', h.Tooltip, 'Position', [131 38 31 22]);
+    'BackgroundColor', [0.9 0.9 0.9], 'Tooltip', h.Tooltip, 'Position', [141 40 31 22]);
  
 % didt
 h = uilabel(hPanel);
 h.HorizontalAlignment = 'right';
-h.Position = [12 10 113 22];
+h.Position = [2 10 112 22];
 h.Text = 'Realized di/dt (A/µs)';
 h.Tooltip = {'Coil current gradient'};
-hs.didt = uieditfield(hPanel, 'numeric', 'Editable','off', 'Tooltip',  h.Tooltip, ...
-    'BackgroundColor', [0.9 0.9 0.9], 'Position', [131 10 31 22]);
+hs.didt = uieditfield(hPanel, 'text', 'Editable','off', 'Tooltip',  h.Tooltip, ...
+    'BackgroundColor', [0.9 0.9 0.9], 'Position', [116 10 56 22], 'HorizontalAlignment', 'right');
  
 % Basic panel
 hPanel = uipanel(fh);
 hPanel.TitlePosition = 'centertop';
 hPanel.Title = 'Basic Control';
 hPanel.FontWeight = 'bold';
-hPanel.Position = [31 135 230 152];
+hPanel.Position = [31 130 230 178];
  
 % amplitude
 h = uilabel(hPanel);
 h.HorizontalAlignment = 'right';
-h.Position = [78 95 80 22];
+h.Position = [78 125 80 22];
 h.Text = 'Amplitude (%)';
 h.Tooltip = {'Stimulation amplitude in percent'};
 hs.amplitude = uispinner(hPanel, 'Limits', [0 100], 'RoundFractionalValues', 'on', ...
-    'ValueChangedFcn', cb("amplitude"), 'Tooltip', h.Tooltip, 'Position',  [167 95 53 22]);
+    'ValueChangedFcn', cb("amplitude"), 'Tooltip', h.Tooltip, 'Position',  [167 125 53 22]);
  
 % mode
 h = uilabel(hPanel);
 h.HorizontalAlignment = 'right';
-h.Position = [47 67 80 22];
+h.Position = [47 97 80 22];
 h.Text = 'Mode';
 h.Tooltip = {'Stimulator mode value other than "Standard" only available for MagOption'};
-hs.mode = uidropdown(hPanel, 'Position', [136 67 84 22],  'Items', "Standard", ...
+hs.mode = uidropdown(hPanel, 'Position', [136 97 84 22],  'Items', "Standard", ...
     'Value', 'Standard', 'BackgroundColor',  [1 1 1], 'ValueChangedFcn', cb("mode"));
-
-% Waveform
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [44 39 59 22];
-h.Text = 'Waveform';
-hs.waveform = uidropdown(hPanel, 'Position', [110 39 110 22], 'Items', "Biphasic", ...
-    'Value', 'Biphasic', 'BackgroundColor',  [1 1 1], 'ValueChangedFcn', cb("waveform"));
 
 % Current Direction
 h = uilabel(hPanel);
 h.HorizontalAlignment = 'right';
-h.Position = [36 10 96 22];
+h.Position = [36 69 96 22];
 h.Text = 'Current Direction';
 hs.currentDirection = uidropdown(hPanel, 'Items', ["Normal" "Reverse"], ...
-    'Value', 'Normal', 'Position',  [140 10 80 22], 'BackgroundColor',  [1 1 1], ...
+    'Value', 'Normal', 'Position',  [140 69 80 22], 'BackgroundColor',  [1 1 1], ...
     'ValueChangedFcn', cb("currentDirection"));
- 
+
+% Waveform
+h = uilabel(hPanel);
+h.HorizontalAlignment = 'right';
+h.Position = [44 40 59 22];
+h.Text = 'Waveform';
+hs.waveform = uidropdown(hPanel, 'Position', [110 40 110 22], 'Items', "Biphasic", ...
+    'Value', 'Biphasic', 'BackgroundColor',  [1 1 1], 'ValueChangedFcn', cb("waveform"));
+
+% BARatio
+hs.BARatioL = uilabel(hPanel);
+hs.BARatioL.Position = [96 10 86 22];
+hs.BARatioL.Text = 'Pulse B/A Ratio';
+hs.BARatioL.Tooltip = 'Amplitude ratio of Pulse B over Pulse A for Twin mode';
+hs.BARatio = uieditfield(hPanel, 'numeric');
+hs.BARatio.Limits = [0.2 5];
+hs.BARatio.Tooltip = hs.BARatioL.Tooltip;
+hs.BARatio.Position = [184 10 36 22];
+hs.BARatio.Value = 1;
+hs.BARatio.ValueDisplayFormat = '%.2g';
+hs.BARatio.ValueChangedFcn = cb("BARatio");
+
 % Burst Panel
 hPanel = uipanel(fh);
 hPanel.TitlePosition = 'centertop';
@@ -247,7 +260,7 @@ trainPanel.TitlePosition = 'centertop';
 trainPanel.Title = 'Train Control';
 trainPanel.FontWeight = 'bold';
 trainPanel.Tooltip = {'Train parameters only sent to stimulator after "Set Train"'};
-trainPanel.Position = [296 22 171 231];
+trainPanel.Position = [280 22 182 231];
  
 % RepRate
 h = uilabel(trainPanel);
@@ -313,38 +326,24 @@ h.HorizontalAlignment = 'right';
 h.Position = [2 71 138 22];
 h.Text = 'Prior Warning Sound';
 h.Tooltip =  {'When on, a beep will sound 2 seconds before each train'};
-hs.PriorWarningSound = uicheckbox(trainPanel);
-hs.PriorWarningSound.Tooltip = h.Tooltip;
-hs.PriorWarningSound.Text = '';
-hs.PriorWarningSound.Position = [146 71 22 22];
-hs.PriorWarningSound.Value = true;
-hs.PriorWarningSound.ValueChangedFcn = cb("PriorWarningSound");
+hs.PriorWarningSound = uicheckbox(trainPanel, 'Tooltip', h.Tooltip, 'Text', '', ...
+    'Position', [146 71 22 22], 'Value', true, 'ValueChangedFcn', cb("PriorWarningSound"));
  
-% TotalTime
+% trainTime
 h = uilabel(trainPanel);
 h.HorizontalAlignment = 'right';
 h.Position = [15 43 80 22];
 h.Text = 'Total Time';
 h.Tooltip = {'Total time to run the sequence, based on above parameters'};
-hs.TotalTime = uieditfield(trainPanel, 'text');
-hs.TotalTime.Editable = 'off';
-hs.TotalTime.BackgroundColor = [0.9 0.9 0.9];
-hs.TotalTime.Tooltip = h.Tooltip;
-hs.TotalTime.Position = [102 43 56 22];
-hs.TotalTime.Value = '00:17';
-hs.TotalTime.HorizontalAlignment = 'right';
+hs.trainTime = uieditfield(trainPanel, 'text', 'Editable', 'off', ...
+    'BackgroundColor', [0.9 0.9 0.9], 'Tooltip', h.Tooltip, 'Value', '00:14', ...
+    'Position', [102 43 56 22], 'HorizontalAlignment', 'right');
  
 hs.fireTrain = uibutton(trainPanel, 'push');
 hs.fireTrain.ButtonPushedFcn = cb("fireTrain");
 hs.fireTrain.Tooltip = {'Start / Stop train sequence'};
-hs.fireTrain.Position = [92 10 64 22];
+hs.fireTrain.Position = [51 10 80 22];
 hs.fireTrain.Text = 'Start Train';
- 
-hs.setTrain = uibutton(trainPanel, 'push');
-hs.setTrain.ButtonPushedFcn = cb("setTrain");
-hs.setTrain.Tooltip = {'Send train parameters to stimulator '};
-hs.setTrain.Position = [12 10 64 22];
-hs.setTrain.Text = 'Set Train';
 
 guidata(fh, hs);
 %%
