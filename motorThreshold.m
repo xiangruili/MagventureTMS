860fbcb23a40e132d3594a209398b8df9e14470c
xiangruili <xiangrui.li@gmail.com>
2025-08-01 17:11:59 -0400

    Update motorThreshold.m
    Use the new RTBoxADCd.m;
    Answers shown on figure, rather than questdlg;
    Support key board answer too.

--- motorThreshold.m
+++ motorThreshold.m
@@ -4,19 +4,19 @@ function motorThreshold(startAmp)
 %  not provided, the current amplitude on the stimulator will be used if it is
 %  greater than 30, otherwise 65 will be the starting amplitude.
 % 
-% When the popup dialog asks if you see motor response, click "Yes" or "No", and
-% the amplitude will be adjusted accordingly for the next trial. In case you are
-% unsure if there is a response, or the stimulation target needs to be adjusted,
-% click "Retry" to keep the amplitude unchanged for the next trial. The code will
-% set the default choice based on the trace, and you can press Enter key if the
-% default choice is correct.
+% When asked if you see motor response, click "Yes" or "No" (or key press y or
+% n), and the amplitude will be adjusted accordingly for the next trial. In case
+% you are unsure for a response, or the stimulation target needs to be adjusted,
+% click "Retry" (or key press r) to keep the amplitude unchanged for the next
+% trial. The code will set the default choice based on the trace, and you can
+% press Spacebar if the default choice is correct.
 % 
 % When the estimate converges, the protocol will stop, and threshold will be
-% shown on the title of the figure.
+% shown on the title of the figure, as well as in Command Window.
 % 
-% Closing the dialog will stop the test.
+% Closing the figure will stop the test.
 
-% 250330 v1 by Xiangrui.Li@gmail.com
+% 250330 v1 by Xiangrui.Li at gmail.com
 
 T = TMS;
 if nargin>1
@@ -28,45 +28,58 @@ end
 T.enabled = true;
 step = 4; btn0 = ''; i = 1;
 
-clear RTBoxADC;
-dur = 0.08;
-RTBoxADC('duration', dur);
-RTBoxADC('channel', 'dif', 200);
-RTBoxADC('Start'); pause(dur+0.05);
-[y, t] = RTBoxADC('read');
-iSig = t>0.01; iBase = t>0.05; iResp = t>0.02 & t<0.05;
+RTBoxADCd('flush');
+[y, ms] = RTBoxADCd('read'); % 288 samples (80 ms) at 3600Hz
+iBase = ms>50; iResp = ms>20 & ms<50;
 
-figure(77); clf;
+fh = figure(77); clf;
 res = get(0, 'ScreenSize');
-set(gcf, 'Position', [40 res(4)-440 1200 400],  'Name', 'Motor Threshold', ...
-  'ToolBar', 'none', 'MenuBar', 'none', 'NumberTitle', 'off');
-h = plot(t*1000, y); h.LineWidth = 1;
-ms = dur*1000; xlim([-5 ms]); xlabel('ms'); xticks(0:10:ms); 
-ylim([-1 1]*3); yticks([]);
-hold on; plot(-4*[1 1], [0.8 1.8], '-k', 'LineWidth', 2); text(-3, 1.3, '1mV');
+set(fh, 'Position', [40 res(4)-440 1200 400],  'Name', 'Motor Threshold Test', ...
+    'ToolBar', 'none', 'MenuBar', 'none', 'NumberTitle', 'off', 'Resize', 'off', ...
+    'Color', 'w', 'WindowKeyPressFcn', @key_cb);
+ax = axes(fh, "Position", [0.05 0.15 0.82 0.7], 'Box', 'off');
+h = plot(ax, ms, bandpass(y)); h.LineWidth = 1;
+xlim([-5 80]); xlabel('ms'); xticks(0:10:80); 
+ylim([-1 1]*3); 
+ax.YAxis.Visible = 'off';
+ax.Box = 'off';
+hold on; plot(ax, -5*[1 1], [0.8 1.8], '-k', 'LineWidth', 2); text(-4.5, 1.3, '1mV');
+mouse_cb = @(o,~) set(fh, 'UserData', o.String);
+p = [fh.Position(3)-72 fh.Position(4)-120 48 24];
+hs(4) = uicontrol(fh, 'Style', 'text', 'string', 'Motor Response?', 'FontSize', 10,  ...
+    'Position', [p(1)-56 p(2) p(3)+72 p(4)], 'BackgroundColor','w', 'HorizontalAlignment',  'right');
+p(2) = p(2) - 50; hs(1) = uicontrol(fh, 'Style', 'pushbutton', 'Position', p, 'Callback', mouse_cb, 'string', 'Yes');
+p(2) = p(2) - 50; hs(2) = uicontrol(fh, 'Style', 'pushbutton', 'Position', p, 'Callback', mouse_cb, 'string', 'No');
+p(2) = p(2) - 50; hs(3) = uicontrol(fh, 'Style', 'pushbutton', 'Position', p, 'Callback', mouse_cb, 'string', 'Retry');
+set(hs, 'Visible', 'off');
 
 while 1
     T.amplitude = amp;
     pause(2+rand*2);
     T.firePulse;
 
-    RTBoxADC('Start'); pause(dur+0.05);
-    y = detrend(RTBoxADC('read') * 1000);
-    y(iSig) = bandpass(y(iSig), [5 500], 3600); % leave trigger artifact
-    figure(77); h.YData = y; drawnow;
+    y = RTBoxADCd('read');
+    y(33:end) = bandpass(y(33:end)); % leave trigger artifact
+    try h.YData = y; drawnow; catch, return; end
 
     ratio = std(y(iResp)) / std(y(iBase));
-    if ratio>3, def = "Yes";
-    elseif i<2, def = "Retry";
-    elseif ratio<1.2, def = "No";
-    else, def = "Retry";
+    if ratio>3, dft = 1;
+    elseif i<2, dft = 3;
+    elseif ratio<1.2, dft = 2;
+    else, dft = 3;
     end
-    btn = questdlg("See motor response?", "Question", "Yes", "No", "Retry", def);
-    if isempty(btn)
-        break;
-    elseif btn=="Retry"
-        continue;
-    elseif btn=="Yes"
+    fh.UserData = '';
+    set(hs, 'Visible', 'on');
+    figure(fh); uicontrol(hs(dft)); drawnow;
+    while isvalid(fh) && isempty(fh.UserData), pause(0.1); end
+    if ~isvalid(fh)
+        fprintf(2, 'Motor threshold test stopped.\n');
+        return;
+    end
+    set(hs, 'Visible', 'off');
+    btn = fh.UserData;
+    if btn=="Retry", continue; end
+    if btn=="Yes"
         if step<1, thre = amp;   break; else, amp = amp - step; end
     elseif btn=="No"
         if step<1, thre = amp+1; break; else, amp = amp + step; end
@@ -75,20 +88,17 @@ while 1
     fprintf(" Trial %2i: amp=%2i, response=%s\n", i, T.amplitude(1), btn);
     btn0 = btn; i = i + 1;
 end
-if isempty(btn), fprintf(2, 'Motor threshold test stopped.\n');
-else, title(h.Parent, sprintf(" Motor threshold is %i\n", thre));
+title(ax, sprintf(" Motor threshold is %i\n", thre));
+
+function y = bandpass(y)
+n = numel(y);
+y = fft(y);
+i = round([5 500]/3600*n + 1);
+y([1:i(1) i(2):n+2-i(2) n+2-i(1):n]) = 0;
+y = real(ifft(y));
+
+function key_cb(fh, evt)
+if     evt.Key == "y", fh.UserData = "Yes";
+elseif evt.Key == "n", fh.UserData = "No";
+elseif evt.Key == "r", fh.UserData = "Retry";
 end
-
-function x = bandpass(x, band, fs)
-% Apply bandpass filter to signal x (row or column vector).
-% The band input is [hp lp] in Hz, and fs is sampling rate in Hz
-%  y = bandpass(x, [5 500], fs); % hp=5Hz, lp=500Hz
-%  y = bandpass(x, [0 500], fs); % lowpass only
-%  y = bandpass(x, [5 inf], fs); % highpass only
-
-n = numel(x);
-x = fft(x);
-i = round(band/fs*n + 1);
-x([1:i(1) n+2-i(1):n]) = 0; % always remove mean: cufoff too steep? 
-if i(2)>1 && i(2)<n, x(i(2):n+2-i(2)) = 0; end
-x = real(ifft(x));
