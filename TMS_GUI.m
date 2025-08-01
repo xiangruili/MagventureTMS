d08ff8442467f7760b5fdea325d2972e7c080048
xiangruili <xiangrui.li@gmail.com>
2025-08-01 17:15:41 -0400

    Update TMS_GUI.m
    simplify the code by using addLabel()

--- TMS_GUI.m
+++ TMS_GUI.m
@@ -77,29 +77,28 @@ hs.IPI.Parent.Enable = st;
 if T.temperature>40, clr = 'r'; else, clr = [1 1 1]*0.9; end
 hs.temperature.BackgroundColor = clr;
 
-%% show trace continuously
-function EMG_check(~, ~)
-clear RTBoxADC;
-RTBoxADC('channel', 'dif', 200);
-RTBoxADC;
-fh = figure(3);
-fh.Position = [52 472 1200 420];
-
 %% callback for most UI components
 function guiCallback(h, ~, tag)
 T = TMS;
 if tag == "enable", T.enabled = ~T.enabled;
+elseif tag == "disconnect"; T.disconnect; set(ancestor(h,'figure'), 'Name', 'NotConnected');
 elseif ismethod(T, tag), T.(tag);
 elseif isprop(T, tag), T.(tag) = h.Value;
 elseif isfield(T.train, tag), T.train.(tag) = h.Value;
 else, disp(h); error("Undefined callback");
 end
 
+%% add label to left of a uicontrol, give it same tooltip
+function lbl = addLabel(h, label)
+lbl = uilabel(h.Parent, 'Text', label, 'Tooltip', h.Tooltip, 'Horizontal', 'Right');
+pos = h.Position;
+lbl.Position = [1 pos(2) pos(1)-6 pos(4)];
+
 %% Create GUI
 function fh = createGUI()
 % Create uifigure and hide until all components are created
 fh = uifigure('Visible', 'off', 'AutoResizeChildren', 'off', 'Name', '', ...
-    'Position', [100 100 489 406], 'Resize', 'off', 'Tag', 'MagventureGUI');
+    'Position', [100 100 490 406], 'Resize', 'off', 'Tag', 'MagventureGUI');
 try fh.Icon =  [fileparts(mfilename) '/TIcon.png']; catch, end
 
 CLN = onCleanup(@()set(fh,'Visible','on')); % Show figure after done or error
@@ -124,222 +123,114 @@ uilabel(fh, 'Text', 'Disabled', 'FontWeight', 'bold', ...
 hs.enabled = uilabel(fh, 'Text', 'Enabled', 'FontWeight', 'bold', 'Visible', 'off', ...
     'HorizontalAlignment', 'center', 'BackgroundColor', 'g', 'Position', [31 362 62 32]);
  
-h = uibutton(fh, 'push');
-h.Text = char(11096); % char(0x23FB) for power button
-h.FontSize = 14;
-h.Position = [94 362 32 32];
-h.ButtonPushedFcn = cb("enable");
-h.Tooltip = {'Push to enable/disable stimulation'};
+% char(0x23FB) for power button
+h = uibutton(fh, 'push', 'Text', char(11096), 'FontSize', 14, 'Position', [94 362 32 32], ...
+    'ButtonPushedFcn', cb("enable"), 'Tooltip', 'Push to enable/disable stimulation');
  
-hs.fire = uibutton(fh, 'push', 'ButtonPushedFcn', cb("firePulse"), ...
-    'FontWeight', 'bold', 'Text', 'Trig', ...
-    'Tooltip', {'Trigger a pulse or burst'}, 'Position', [158 362 98 32]);
+hs.fire = uibutton(fh, 'push', 'ButtonPushedFcn', cb("firePulse"), 'FontWeight', 'bold', ...
+    'Text', 'Trig', 'Tooltip', {'Trigger a pulse or burst'}, 'Position', [136 362 98 32]);
  
 uibutton(fh, 'push', 'ButtonPushedFcn', @(~,~)motorThreshold, 'Text', 'Motor Threshold', ...
-    'Tooltip', {'Start motor threshold estimate'}, 'Position', [158 322 98 32]);
+    'Tooltip', {'Start motor threshold estimate'}, 'Position', [136 322 98 32]);
 
-uibutton(fh, 'push', 'ButtonPushedFcn', @EMG_check, 'Text', 'EMG Check', ...
+uibutton(fh, 'push', 'ButtonPushedFcn', @(~,~)RTBoxADCd, 'Text', 'EMG Check', ...
     'Tooltip', {'Show continuous EMG trace'}, 'Position', [31 322 88 32]);
 
-% Coil panel
-hPanel = uipanel(fh, 'TitlePosition', 'centertop', 'Title', 'Coil Status', ...
-    'FontWeight', 'bold', 'Position', [280 267 182 122]);
+% Basic Control
+hPanel = uipanel(fh, 'Title', 'Basic Control', 'FontWeight', 'bold', 'Position', [31 130 206 180]);
  
-% coil type
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [13 70 77 22];
-h.Text = 'Type/Number';
-hs.CoilType = uieditfield(hPanel, 'text', 'Position', [95 70 77 22], ...
-    'Editable', 'off', 'BackgroundColor', [0.9 0.9 0.9], ...
-    'Tooltip', {'Connected coil type or number'});
- 
-% temperature
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [14 40 121 22];
-h.Text = 'Temperature (°C)';
-h.Tooltip = {'Coil temperature in Celsius'};
-hs.temperature = uieditfield(hPanel, 'numeric', 'Editable', 'off', ...
-    'BackgroundColor', [0.9 0.9 0.9], 'Tooltip', h.Tooltip, 'Position', [141 40 31 22]);
- 
-% didt
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [2 10 112 22];
-h.Text = 'Realized di/dt (A/µs)';
-h.Tooltip = {'Coil current gradient'};
-hs.didt = uieditfield(hPanel, 'text', 'Editable','off', 'Tooltip',  h.Tooltip, ...
-    'BackgroundColor', [0.9 0.9 0.9], 'Position', [116 10 56 22], 'HorizontalAlignment', 'right');
- 
-% Basic panel
-hPanel = uipanel(fh);
-hPanel.TitlePosition = 'centertop';
-hPanel.Title = 'Basic Control';
-hPanel.FontWeight = 'bold';
-hPanel.Position = [31 130 230 178];
- 
-% amplitude
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [78 125 80 22];
-h.Text = 'Amplitude (%)';
-h.Tooltip = {'Stimulation amplitude in percent'};
 hs.amplitude = uispinner(hPanel, 'Limits', [0 100], 'RoundFractionalValues', 'on', ...
-    'ValueChangedFcn', cb("amplitude"), 'Tooltip', h.Tooltip, 'Position',  [167 125 53 22]);
- 
-% mode
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [47 97 80 22];
-h.Text = 'Mode';
-h.Tooltip = {'Stimulator mode value other than "Standard" only available for MagOption'};
-hs.mode = uidropdown(hPanel, 'Position', [136 97 84 22],  'Items', "Standard", ...
-    'Value', 'Standard', 'BackgroundColor',  [1 1 1], 'ValueChangedFcn', cb("mode"));
+    'ValueChangedFcn', cb("amplitude"), 'Position',  [143 130 53 22], ...
+    'Tooltip', 'Stimulation amplitude in percent');
+addLabel(hs.amplitude, 'Amplitude (%)');
 
-% Current Direction
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [36 69 96 22];
-h.Text = 'Current Direction';
+h.Tooltip = {'Stimulator mode other than "Standard" only available for MagOption'};
+hs.mode = uidropdown(hPanel, 'Position', [112 100 84 22],  'Items', "Standard", ...
+    'Value', 'Standard', 'BackgroundColor',  [1 1 1], 'ValueChangedFcn', cb("mode"), ...
+    'Tooltip', 'Stimulator mode other than "Standard" only available for MagOption');
+addLabel(hs.mode, 'Mode');
+
 hs.currentDirection = uidropdown(hPanel, 'Items', ["Normal" "Reverse"], ...
-    'Value', 'Normal', 'Position',  [140 69 80 22], 'BackgroundColor',  [1 1 1], ...
+    'Value', 'Normal', 'Position',  [116 70 80 22], 'BackgroundColor',  [1 1 1], ...
     'ValueChangedFcn', cb("currentDirection"));
+addLabel(hs.currentDirection, 'Current Direction');
 
-% Waveform
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [44 40 59 22];
-h.Text = 'Waveform';
-hs.waveform = uidropdown(hPanel, 'Position', [110 40 110 22], 'Items', "Biphasic", ...
+hs.waveform = uidropdown(hPanel, 'Position', [86 40 110 22], 'Items', "Biphasic", ...
     'Value', 'Biphasic', 'BackgroundColor',  [1 1 1], 'ValueChangedFcn', cb("waveform"));
+addLabel(hs.waveform, 'Waveform');
 
-% BARatio
-hs.BARatioL = uilabel(hPanel);
-hs.BARatioL.Position = [96 10 86 22];
-hs.BARatioL.Text = 'Pulse B/A Ratio';
-hs.BARatioL.Tooltip = 'Amplitude ratio of Pulse B over Pulse A for Twin mode';
-hs.BARatio = uieditfield(hPanel, 'numeric');
-hs.BARatio.Limits = [0.2 5];
-hs.BARatio.Tooltip = hs.BARatioL.Tooltip;
-hs.BARatio.Position = [184 10 36 22];
-hs.BARatio.Value = 1;
-hs.BARatio.ValueDisplayFormat = '%.2g';
-hs.BARatio.ValueChangedFcn = cb("BARatio");
+hs.BARatio = uieditfield(hPanel, 'numeric', 'Position', [160 10 36 22], ...
+    'Limits', [0.2 5], 'Value', 1, 'ValueChangedFcn', cb("BARatio"), ...
+    'Tooltip', 'Amplitude ratio of Pulse B over Pulse A for Twin mode');
+hs.BARatioL = addLabel(hs.BARatio, 'Pulse B/A Ratio');
 
-% Burst Panel
-hPanel = uipanel(fh);
-hPanel.TitlePosition = 'centertop';
-hPanel.Title = 'Burst Parameters';
-hPanel.FontWeight = 'bold';
-hPanel.Position = [31 22 230 93];
-hPanel.Tooltip = {'Only active with Waveform of "Biphasic Burst"'};
+% Burst Parameters
+hPanel = uipanel(fh, 'Title', 'Burst Parameters', 'FontWeight', 'bold', 'Position', [31 22 206 90], ...
+    'Tooltip', 'Only active with Waveform of "Biphasic Burst"');
  
-% burstPulses
-h = uilabel(hPanel, 'HorizontalAlignment', 'right', 'Position', [102 42 72 22], ...
-    'Text', 'Burst Pulses', 'Tooltip', {'Number of pulses in a burst'});
 hs.burstPulses = uispinner(hPanel, 'Limits', [2 5], 'RoundFractionalValues', 'on', ...
-    'ValueChangedFcn', cb("burstPulses"), 'Tooltip', h.Tooltip, 'Position', [181 42 42 22]);
+    'ValueChangedFcn', cb("burstPulses"), 'Position', [157 40 42 22], ...
+    'Tooltip', 'Number of pulses in a burst');
+addLabel(hs.burstPulses, 'Burst Pulses');
  
-% IPI
-h = uilabel(hPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [49 11 132 22];
-h.Text = 'Inter Pulse Interval (ms)';
-h.Tooltip = {'Duration between the beginning of the first pulse to the beginning of the second pulse'};
-hs.IPI = uieditfield(hPanel, 'numeric', 'Limits', [0.5 100], 'ValueChangedFcn', cb("IPI"), ...
-    'Tooltip', h.Tooltip, 'Position', [187 11 34 22], 'Value', 10);
+hs.IPI = uieditfield(hPanel, 'numeric', 'ValueChangedFcn', cb("IPI"), ...
+    'Position', [163 10 34 22], 'Value', 10, ...
+    'Tooltip', 'Duration between the beginning of the first pulse to the beginning of the second pulse');
+addLabel(hs.IPI, 'Inter Pulse Interval (ms)');
+
+% Coil Status
+hPanel = uipanel(fh, 'Title', 'Coil Status', 'FontWeight', 'bold', 'Position', [256 270 206 122]);
  
-% Train Panel
-trainPanel = uipanel(fh);
-trainPanel.TitlePosition = 'centertop';
-trainPanel.Title = 'Train Control';
-trainPanel.FontWeight = 'bold';
-trainPanel.Tooltip = {'Train parameters only sent to stimulator after "Set Train"'};
-trainPanel.Position = [280 22 182 231];
+hs.CoilType = uieditfield(hPanel, 'text', 'Position', [94 70 100 22], 'Editable', 'off', ...
+    'BackgroundColor', [1 1 1]*0.9, 'HorizontalAlignment', 'right', ...
+    'Tooltip', {'Connected coil type or number'});
+addLabel(hs.CoilType, 'Type/Number');
  
-% RepRate
-h = uilabel(trainPanel);
-h.HorizontalAlignment = 'right';
-h.Tooltip = {'Number of pulses per second'};
-h.Position = [28 183 90 22];
-h.Text = 'Rep. Rate (pps)';
-hs.RepRate = uieditfield(trainPanel, 'numeric');
-hs.RepRate.Limits = [0.1 100];
-hs.RepRate.Tooltip = h.Tooltip;
-hs.RepRate.Position = [125 183 36 22];
-hs.RepRate.Value = 1;
-hs.RepRate.ValueDisplayFormat = '%.4g';
-hs.RepRate.ValueChangedFcn = cb("RepRate");
+hs.temperature = uieditfield(hPanel, 'numeric', 'Editable', 'off', 'Position', [163 40 31 22], ...
+    'BackgroundColor', [1 1 1]*0.9, 'Tooltip', 'Coil temperature in Celsius');
+addLabel(hs.temperature, 'Temperature (°C)');
  
-% PulsesInTrain
-h = uilabel(trainPanel);
-h.HorizontalAlignment = 'right';
-h.Tooltip = {'Number of pulses or bursts in each train'};
-h.Position = [34 155 84 22];
-h.Text = 'Pulses in Train';
-hs.PulsesInTrain = uieditfield(trainPanel, 'numeric');
-hs.PulsesInTrain.Limits = [1 2000];
-hs.PulsesInTrain.RoundFractionalValues = 'on';
-hs.PulsesInTrain.Tooltip = h.Tooltip;
-hs.PulsesInTrain.Position = [125 155 36 22];
-hs.PulsesInTrain.Value = 5;
-hs.PulsesInTrain.ValueChangedFcn = cb("PulsesInTrain");
+hs.didt = uieditfield(hPanel, 'text', 'Editable','off', 'Tooltip',  'Coil current gradient', ...
+    'BackgroundColor', [1 1 1]*0.9, 'Position', [138 10 56 22], 'HorizontalAlignment', 'right');
+addLabel(hs.didt, 'Realized di/dt (A/µs)');
  
-% NumberOfTrains
-h = uilabel(trainPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [21 127 97 22];
-h.Text = 'Number of Trains';
-h.Tooltip = {'Total amount of trains arriving in one sequence'};
-hs.NumberOfTrains = uieditfield(trainPanel, 'numeric');
-hs.NumberOfTrains.Limits = [1 500];
-hs.NumberOfTrains.RoundFractionalValues = 'on';
-hs.NumberOfTrains.Tooltip = h.Tooltip;
-hs.NumberOfTrains.Position = [125 127 36 22];
-hs.NumberOfTrains.Value = 3;
-hs.NumberOfTrains.ValueChangedFcn = cb("NumberOfTrains");
+% Train Control
+hPanel = uipanel(fh, 'Title', 'Train Control', 'FontWeight', 'bold', 'Position', [256 22 206 234]);
  
-% ITI
-h = uilabel(trainPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [1 99 119 22];
-h.Text = 'Inter Train Interval (s)';
-h.Tooltip = {'The time interval between two trains described as'; ...
-    'the time period between the last pulse in the first'; ...
-    'train to the first pulse in the next train'};
-hs.ITI = uieditfield(trainPanel, 'numeric');
-hs.ITI.Limits = [0.1 300];
-hs.ITI.Tooltip = h.Tooltip;
-hs.ITI.Position = [125 99 36 22];
-hs.ITI.Value = 1;
-hs.ITI.ValueDisplayFormat = '%.4g';
-hs.ITI.ValueChangedFcn = cb("ITI");
+hs.RepRate = uieditfield(hPanel, 'numeric', 'Value', 1, 'Position', [158 183 36 22], ...
+    'ValueChangedFcn', cb("RepRate"), 'Tooltip', 'Number of pulses per second');
+addLabel(hs.RepRate, 'Rep. Rate (pps)');
  
-% PriorWarningSound
-h = uilabel(trainPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [2 71 138 22];
-h.Text = 'Prior Warning Sound';
-h.Tooltip =  {'When on, a beep will sound 2 seconds before each train'};
-hs.PriorWarningSound = uicheckbox(trainPanel, 'Tooltip', h.Tooltip, 'Text', '', ...
-    'Position', [146 71 22 22], 'Value', true, 'ValueChangedFcn', cb("PriorWarningSound"));
+hs.PulsesInTrain = uieditfield(hPanel, 'numeric', 'Limits', [1 2000], ...
+    'RoundFractionalValues', 'on', 'Position', [158 155 36 22], 'Value', 5, ...
+    'ValueChangedFcn', cb("PulsesInTrain"), ...
+    'Tooltip', 'Number of pulses or bursts in each train');
+addLabel(hs.PulsesInTrain, 'Pulses in Train');
  
-% trainTime
-h = uilabel(trainPanel);
-h.HorizontalAlignment = 'right';
-h.Position = [15 43 80 22];
-h.Text = 'Total Time';
-h.Tooltip = {'Total time to run the sequence, based on above parameters'};
-hs.trainTime = uieditfield(trainPanel, 'text', 'Editable', 'off', ...
-    'BackgroundColor', [0.9 0.9 0.9], 'Tooltip', h.Tooltip, 'Value', '00:14', ...
-    'Position', [102 43 56 22], 'HorizontalAlignment', 'right');
+hs.NumberOfTrains = uieditfield(hPanel, 'numeric', 'Limits', [1 500], 'Value', 3, ...
+    'RoundFractionalValues', 'on', 'Position', [158 127 36 22], 'ValueChangedFcn', cb("NumberOfTrains"), ...
+    'Tooltip', 'Total amount of trains arriving in one sequence');
+addLabel(hs.NumberOfTrains, 'Number of Trains');
  
-hs.fireTrain = uibutton(trainPanel, 'push');
-hs.fireTrain.ButtonPushedFcn = cb("fireTrain");
-hs.fireTrain.Tooltip = {'Start / Stop train sequence'};
-hs.fireTrain.Position = [51 10 80 22];
-hs.fireTrain.Text = 'Start Train';
+hs.ITI = uieditfield(hPanel, 'numeric', 'Limits', [0.1 300], 'Value', 1, ...
+    'Position', [158 99 36 22], 'ValueChangedFcn', cb("ITI"), ...
+    'Tooltip', {'The time interval between two trains described as'; ...
+        'the time period between the last pulse in the first'; ...
+        'train to the first pulse in the next train'});
+addLabel(hs.ITI, 'Inter Train Interval (s)');
+
+hs.PriorWarningSound = uicheckbox(hPanel, 'Text', '', 'Position', [176 71 22 22], ...
+    'Value', true, 'ValueChangedFcn', cb("PriorWarningSound"), ...
+    'Tooltip', 'When on, a beep will sound 2 seconds before each train');
+addLabel(hs.PriorWarningSound, 'Prior Warning Sound');
+ 
+hs.trainTime = uieditfield(hPanel, 'text', 'Editable', 'off', 'BackgroundColor', [1 1 1]*0.9, ...
+    'Value', '00:14', 'Position', [138 43 56 22], 'HorizontalAlignment', 'right', ...
+    'Tooltip','Total time to run the sequence, based on above parameters');
+addLabel(hs.trainTime, 'Total Time');
+ 
+hs.fireTrain = uibutton(hPanel, 'push', 'ButtonPushedFcn', cb("fireTrain"), ...
+    'Tooltip', 'Start / Stop train sequence', 'Position', [51 10 92 22], 'Text', 'Start Train');
 
 guidata(fh, hs);
 %%
